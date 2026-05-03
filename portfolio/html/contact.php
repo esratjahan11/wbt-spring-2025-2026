edd50ab6904a1d40745ba4037d2e0acae0182056
Israt Jahan Mukti <isratjahanmukti91@gmail.com>
2026-05-03 15:40:34 +0600

    23-52681-2

--- portfolio/html/contact.php
+++ portfolio/html/contact.php
@@ -4,120 +4,76 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Form</title>
-    <link rel="stylesheet" href="../css/Contact.css">
+    <title>Login Form</title>
 </head>
 
 <body>
 
-
-    <!-- Registration Form with Table -->
-    <form action="contact.php" method="post">
+    <!-- Login Form -->
+    <form action="login.php" method="post">
         <fieldset>
-            <legend>Registration Form</legend>
+            <legend>Login Form</legend>
 
             <table>
                 <tr>
-                    <td><label for="fname">First Name:</label></td>
-                    <td><input type="text" id="fname" name="fname" required /></td>
+                    <td><label for="username">Username:</label></td>
+                    <td><input type="text" id="username" name="username" required></td>
                 </tr>
+
                 <tr>
-                    <td><label for="lname">Last Name:</label></td>
-                    <td><input type="text" id="lname" name="lname" required /></td>
-                </tr>
-                <tr>
-                    <td><label>Gender:</label></td>
+                    <td><label for="password">Password:</label></td>
                     <td>
-                        <input type="radio" name="gender" value="male" />Male
-                        <input type="radio" name="gender" value="female" />Female
-                    </td>
-                </tr>
-                <tr>
-                    <td><label for="email">Email:</label></td>
-                    <td><input type="email" id="email" name="email" required /></td>
-                </tr>
-                <tr>
-                    <td><label>Reason Of Contract</label></td>
-                    <td>
-                        <input
-                            type="radio"
-                            name="Projects"
-                            value="Projects" />Projects
-                        <input
-                            type="radio"
-                            name="Thesis"
-                            value="Thesis" />Thesis
-                        <input type="radio" name="Job" value="Job" />Job
+                        <input type="password" id="password" name="password"
+                            minlength="8" maxlength="8" required>
                     </td>
                 </tr>
 
                 <tr>
-                    <td><label>Topics:</label></td>
-                    <td>
-                        <input type="checkbox" name="web" value="Web Development" required /> Web
-                        Development
-                        <input type="checkbox" name="mobile" value="Mobile Development" /> Mobile
-                        Development
-                        <input type="checkbox" name="ai" value="AI/ML Development" /> AI/ML
-                        Development
-                    </td>
+                    <td><label for="gmail">Gmail:</label></td>
+                    <td><input type="email" id="gmail" name="gmail" required></td>
                 </tr>
+
                 <tr>
-                    <td><label>Consultation date:</label></td>
+                    <td><label>Reason Of Contact:</label></td>
                     <td>
-                        <input type="date" id="start" name="Consultation-date" />
+                        <input type="radio" name="reason" value="Projects"> Projects
+                        <input type="radio" name="reason" value="Thesis"> Thesis
+                        <input type="radio" name="reason" value="Job"> Job
                     </td>
                 </tr>
 
                 <tr>
                     <td></td>
                     <td>
-                        <input type="submit" name="confirm" value="confirm" />
-                        <input type="reset" />
+                        <input type="submit" name="login" value="Login">
+                        <input type="reset" value="Reset">
                     </td>
                 </tr>
             </table>
+
         </fieldset>
     </form>
 
-
-
-    <a href="index.html">Back to Home</a>
 </body>
 
-</html>\
+</html>
 
 
 <?php
-if (isset($_POST['confirm'])) {
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $email = $_POST['email'];
-    $gender = $_POST['gender'];
-    $consultationDate = $_POST['Consultation-date'];
-    $reasonOfContract = $_POST['Projects'] ?? $_POST['Thesis'] ?? $_POST['Job'] ?? 'Not specified';
-    $topics = [];
+if (isset($_POST['login'])) {
 
-    if (isset($_POST['web'])) {
-        $topics[] = $_POST['web'];
-    }
-    if (isset($_POST['mobile'])) {
-        $topics[] = $_POST['mobile'];
-    }
-    if (isset($_POST['ai'])) {
-        $topics[] = $_POST['ai'];
-    }
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $gmail = $_POST['gmail'];
+    $reason = $_POST['reason'];
 
-    if (empty($fname) || empty($lname) || empty($gender) || empty($email) || empty($consultationDate) || empty($reasonOfContract) || empty($topics)) {
-        echo "Please fill in all required fields.";
+    if (empty($username) || empty($password) || empty($gmail) || empty($reason)) {
+        echo "Please fill all fields.";
     } else {
-        echo "First Name: {$fname} <br>";
-        echo "Last Name: {$lname} <br>";
-        echo "Gender: {$gender}<br>";
-        echo "Email: {$email} <br>";
-        echo "Reason of Contract: {$reasonOfContract} <br>";
-        echo "Topics: { implode(", ", $topics) } <br>";
-        echo "Consultation Date: {$consultationDate} <br>";
+        echo "Username: {$username} <br>";
+        echo "Password: {$password} <br>";
+        echo "Gmail: {$gmail} <br>";
+        echo "Reason Of Contact: {$reason}";
     }
 }
 ?>
